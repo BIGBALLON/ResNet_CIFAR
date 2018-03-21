3941c3688777d7de13a27b91eca5f59bf53613e7
BIGBALLON <605718199@qq.com>
2018-03-21 19:44:15 +0800

    add code & run.sh

---
+++ run.sh
@@ -0,0 +1,33 @@
+# epoch 200 learning rate scheduler 1
+python3 ResNet_keras.py --epochs 200 --stack_n 3 --lr_scheduler 1
+python3 ResNet_keras.py --epochs 200 --stack_n 5 --lr_scheduler 1
+python3 ResNet_keras.py --epochs 200 --stack_n 8 --lr_scheduler 1
+python3 ResNet_keras.py --epochs 200 --stack_n 18 --lr_scheduler 1
+python3 ResNet_keras.py --epochs 200 --stack_n 25 --lr_scheduler 1
+python3 ResNet_keras.py --epochs 200 --stack_n 3  --lr_scheduler 1 --dataset cifar100
+python3 ResNet_keras.py --epochs 200 --stack_n 5  --lr_scheduler 1 --dataset cifar100
+python3 ResNet_keras.py --epochs 200 --stack_n 8  --lr_scheduler 1 --dataset cifar100
+python3 ResNet_keras.py --epochs 200 --stack_n 18 --lr_scheduler 1 --dataset cifar100
+python3 ResNet_keras.py --epochs 200 --stack_n 25 --lr_scheduler 1 --dataset cifar100
+# epoch 200 learning rate scheduler 2
+python3 ResNet_keras.py --epochs 200 --stack_n 3 --lr_scheduler 2
+python3 ResNet_keras.py --epochs 200 --stack_n 5 --lr_scheduler 2
+python3 ResNet_keras.py --epochs 200 --stack_n 8 --lr_scheduler 2
+python3 ResNet_keras.py --epochs 200 --stack_n 18 --lr_scheduler 2
+python3 ResNet_keras.py --epochs 200 --stack_n 25 --lr_scheduler 2
+python3 ResNet_keras.py --epochs 200 --stack_n 3  --lr_scheduler 2 --dataset cifar100
+python3 ResNet_keras.py --epochs 200 --stack_n 5  --lr_scheduler 2 --dataset cifar100
+python3 ResNet_keras.py --epochs 200 --stack_n 8  --lr_scheduler 2 --dataset cifar100
+python3 ResNet_keras.py --epochs 200 --stack_n 18 --lr_scheduler 2 --dataset cifar100
+python3 ResNet_keras.py --epochs 200 --stack_n 25 --lr_scheduler 2 --dataset cifar100
+# epoch 300 learning rate scheduler 3
+python3 ResNet_keras.py --epochs 300 --stack_n 3 --lr_scheduler  3
+python3 ResNet_keras.py --epochs 300 --stack_n 5 --lr_scheduler  3
+python3 ResNet_keras.py --epochs 300 --stack_n 8 --lr_scheduler  3
+python3 ResNet_keras.py --epochs 300 --stack_n 18 --lr_scheduler 3
+python3 ResNet_keras.py --epochs 300 --stack_n 25 --lr_scheduler 3
+python3 ResNet_keras.py --epochs 300 --stack_n 3  --lr_scheduler 3 --dataset cifar100
+python3 ResNet_keras.py --epochs 300 --stack_n 5  --lr_scheduler 3 --dataset cifar100
+python3 ResNet_keras.py --epochs 300 --stack_n 8  --lr_scheduler 3 --dataset cifar100
+python3 ResNet_keras.py --epochs 300 --stack_n 18 --lr_scheduler 3 --dataset cifar100
+python3 ResNet_keras.py --epochs 300 --stack_n 25 --lr_scheduler 3 --dataset cifar100
