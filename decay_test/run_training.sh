0bba43594af4fe6cac6ca0fb5eb5579dbad0c877
bigballon <605718199@qq.com>
2018-06-15 23:18:55 +0800

    add new test script

---
+++ decay_test/run_training.sh
@@ -0,0 +1,10 @@
+python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 1 --count 1
+python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 1 --count 2
+python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 2 --count 1
+python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 2 --count 2
+python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 3 --count 1
+python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 3 --count 2
+python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 4 --count 1
+python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 4 --count 2
+
+
