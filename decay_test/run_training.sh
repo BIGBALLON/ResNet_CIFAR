9845088e801863fdf255b268ba571bbc43818f38
bigballon <605718199@qq.com>
2018-06-17 21:31:12 +0800

    start new training

--- decay_test/run_training.sh
+++ decay_test/run_training.sh
@@ -1,10 +1,14 @@
-python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 1 --count 1
-python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 1 --count 2
-python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 2 --count 1
-python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 2 --count 2
-python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 3 --count 1
-python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 3 --count 2
-python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 4 --count 1
-python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 4 --count 2
-
+# python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 1 --count 1
+# python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 1 --count 2
+# python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 2 --count 1
+# python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 2 --count 2
+# python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 3 --count 1
+# python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 3 --count 2
+# python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 4 --count 1
+# python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar100 --scheduler 4 --count 2
+# 
+python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar10 --scheduler 1 --count 1
+python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar10 --scheduler 2 --count 1
+python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar10 --scheduler 3 --count 1
+python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar10 --scheduler 4 --count 1
 
