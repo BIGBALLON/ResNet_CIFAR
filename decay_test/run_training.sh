1e20142793d709bf8043bf8ad5cd2e63369c5a87
bigballon <605718199@qq.com>
2018-06-20 12:12:48 +0800

    upload new experiments & visualizations

--- decay_test/run_training.sh
+++ decay_test/run_training.sh
@@ -12,4 +12,4 @@
 # python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar10 --scheduler 3 --count 1
 # python ResNet.py --batch_size 128 --epoch 200 --stack_n 8 --dataset cifar10 --scheduler 4 --count 1
 #
-python ResNet.py --batch_size 128 --epoch 400 --stack_n 8 --dataset cifar100 --scheduler 5 --count 1
+python3 ResNet.py --batch_size 128 --epoch 400 --stack_n 8 --dataset cifar10 --scheduler 5 --count 1
